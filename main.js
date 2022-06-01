0b8c171511914c14c549c634c7ce0ab5be40fc29
devgildevan <gildevan87654321@gmail.com>
2022-05-31 23:19:19 -0300

    adicionadoJS

---
+++ main.js
@@ -0,0 +1,24 @@
+/* Abre a fecha o menu quando clicar no icone: com três linha e X*/
+const nav = document.querySelector('#header nav')
+const toggle = document.querySelectorAll('nav .toggle')
+
+for ( const element of toggle) {
+    element.addEventListener( 'click', function(){
+        nav.classList.toggle( 'show')
+    })
+}
+
+/*Quando clicar em um item do menu, esconder o menu */ 
+const links = document.querySelectorAll(' nav ul li a')
+
+    for (const link of links) {
+         link.addEventListener( 'click', function(){
+            nav.classList.remove( 'show')
+        })
+    }
+    
+
+
+
+
+
